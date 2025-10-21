546961d023df7ed2a6306456083b8f1d96030b85
pgonzalezruizgijon <pablo.gonzalez-galvez@iesruizgijon.com>
2025-10-21 22:09:10 +0200

    bisiesto.spec.js modificado

--- Tema 3/EjerciciosDeClase/jasmine-bisiesto/spec/bisiesto.spec.js
+++ Tema 3/EjerciciosDeClase/jasmine-bisiesto/spec/bisiesto.spec.js
@@ -7,14 +7,28 @@ describe("Testeo de la función esBisiesto()", function(){
     {año: 400, valorEsperado:true},
     {año: 1200, valorEsperado:true},
     {año: 1904, valorEsperado:true},
-    {año: 1905, valorEsperado:true},
-    {año: 2015, valorEsperado:true},
-    {año: 2019, valorEsperado:true},
+    {año: 1905, valorEsperado:false},
+    {año: 2015, valorEsperado:false},
+    {año: 2019, valorEsperado:false},
  ];
 
+ /*
  it ("400 debería ser un año bisiesto", function(){
     expect(esBisiesto(400)).toBeTrue();
  });
+
+  it ("400 debería ser un año bisiesto", function(){
+    expect(esBisiesto(1905)).toBeFalse();
+ });
+ */
+
+ datos.forEach(
+   (item) => {
+      it (`El año ${item.año} ${item.valorEsperado? "sí":"no"} debería ser bisiesto`, function() {
+         expect(esBisiesto(item.año)).toEqual(item.valorEsperado);
+      })
+   }
+ )
 
 
 
